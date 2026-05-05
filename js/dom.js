911649bbb4dcfe19de8bb4c5f8518ba6aa11f006
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-05 18:36:24 +0800

    Create dom.js

---
+++ js/dom.js
@@ -0,0 +1,21 @@
+export const dom = {
+  placeFlow: document.getElementById("placeFlow"),
+  recommendGrid: document.getElementById("recommendGrid"),
+
+  heroImage: document.getElementById("heroImage"),
+  heroTitle: document.getElementById("heroTitle"),
+  heroDesc: document.getElementById("heroDesc"),
+
+  sectionTitle: document.getElementById("sectionTitle"),
+  aiNoteText: document.getElementById("aiNoteText"),
+
+  detailView: document.getElementById("detailView"),
+  aiSheet: document.getElementById("aiSheet"),
+
+  searchInput: document.getElementById("searchInput"),
+
+  currentLocationText: document.getElementById("currentLocationText"),
+  aiGreeting: document.getElementById("aiGreeting"),
+  aiUserState: document.getElementById("aiUserState"),
+  aiSuggestionCards: document.getElementById("aiSuggestionCards")
+};
