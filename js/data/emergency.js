f56d428e14e1f1dfba6145029686688929e27799
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-10 13:49:55 +0800

    Create emergency.js

---
+++ js/data/emergency.js
@@ -0,0 +1,69 @@
+export const emergencyData = [
+  {
+    id: "kuching-police-central",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "police",
+    typeLabel: "警察局",
+    name: "Kuching Central Police Station",
+    phone: "082-244444",
+    desc: "旅途中若遇到遺失、報案或突發事件，可聯絡當地警察局。",
+    map: "https://www.google.com/maps/search/?api=1&query=Kuching+Central+Police+Station"
+  },
+  {
+    id: "kuching-fire-rescue",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "fire",
+    typeLabel: "消防局",
+    name: "Kuching Fire & Rescue",
+    phone: "999",
+    desc: "緊急火災、救援或事故支援。",
+    map: "https://www.google.com/maps/search/?api=1&query=Kuching+Fire+Station"
+  },
+  {
+    id: "kuching-sarawak-general-hospital",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "hospital",
+    typeLabel: "醫院",
+    name: "Sarawak General Hospital",
+    phone: "082-276666",
+    desc: "Kuching 主要大型醫療中心。",
+    map: "https://www.google.com/maps/search/?api=1&query=Sarawak+General+Hospital"
+  },
+
+  {
+    id: "sibu-police-central",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "police",
+    typeLabel: "警察局",
+    name: "Sibu Central Police Station",
+    phone: "999",
+    desc: "旅途中若遇到遺失、報案或突發事件，可先聯絡警方。",
+    map: "https://www.google.com/maps/search/?api=1&query=Sibu+Central+Police+Station"
+  },
+  {
+    id: "sibu-fire-station",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "fire",
+    typeLabel: "消防局",
+    name: "Sibu Fire Station",
+    phone: "999",
+    desc: "提供當地緊急救援服務。",
+    map: "https://www.google.com/maps/search/?api=1&query=Sibu+Fire+Station"
+  },
+  {
+    id: "sibu-hospital",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "hospital",
+    typeLabel: "醫院",
+    name: "Sibu Hospital",
+    phone: "084-343333",
+    desc: "Sibu 主要醫療支援單位。",
+    map: "https://www.google.com/maps/search/?api=1&query=Sibu+Hospital"
+  }
+];
