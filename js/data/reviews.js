8c74dea8cb941e8c762b547775462ac377841587
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-10 12:40:03 +0800

    Create reviews.js

---
+++ js/data/reviews.js
@@ -0,0 +1,26 @@
+export const reviews = [
+  {
+    id: "review-sibu",
+
+    city: "sibu",
+
+    name: "Alicia",
+
+    achievement: "探索了 18 個市場",
+
+    title: "Sibu 比我想像中更有生活感",
+
+    description:
+      "市場、夜市和河邊都很自然。",
+
+    place: "Sibu Central Market",
+
+    tags: [
+      "Sibu",
+      "SlowTravel"
+    ],
+
+    image:
+      "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?auto=format&fit=crop&w=1000&q=80"
+  }
+];
