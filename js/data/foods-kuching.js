dd8bd1262d51124450492630c307435103ffa5f0
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-10 12:39:17 +0800

    Create foods-kuching.js

---
+++ js/data/foods-kuching.js
@@ -0,0 +1,29 @@
+import { img } from "./shared.js";
+
+export const kuchingFoods = [
+  {
+    id: "sarawak-laksa",
+    city: "kuching",
+
+    name: "Sarawak Laksa",
+
+    location: "Kuching",
+
+    tag: "#砂拉越叻沙",
+
+    image: img.food
+  },
+
+  {
+    id: "kolo-mee",
+    city: "kuching",
+
+    name: "Kolo Mee",
+
+    location: "Kuching",
+
+    tag: "#乾撈麵",
+
+    image: img.food
+  }
+];
