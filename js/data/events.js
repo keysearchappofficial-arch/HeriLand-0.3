abb949d82ea1f73ec1a9a412e77686c6f4c5b32f
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-10 12:39:39 +0800

    Create events.js

---
+++ js/data/events.js
@@ -0,0 +1,15 @@
+export const events = [
+  {
+    id: "sibu-night-market-walk",
+    city: "sibu",
+
+    title: "Sibu Night Market Walk",
+
+    date: "每日傍晚",
+
+    location: "Sibu Night Market",
+
+    image:
+      "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=1000&q=80"
+  }
+];
