b0ecbee08f099e3fab5326da6e2bd5fb2e961448
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-10 14:13:48 +0800

    Create government.js

---
+++ js/data/government.js
@@ -0,0 +1,99 @@
+export const governmentData = [
+  {
+    id: "kuching-immigration-office",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "immigration",
+    typeLabel: "Immigration",
+    name: "Sarawak Immigration Office",
+    hours: "Mon – Fri · 8:00 AM – 5:00 PM",
+    phone: "082-242133",
+    website: "https://imi.gov.my",
+    map: "https://www.google.com/maps/search/?api=1&query=Sarawak+Immigration+Office+Kuching"
+  },
+  {
+    id: "kuching-jpj",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "jpj",
+    typeLabel: "JPJ",
+    name: "JPJ Kuching",
+    hours: "Mon – Fri · 8:00 AM – 4:30 PM",
+    phone: "082-259900",
+    website: "https://www.jpj.gov.my",
+    map: "https://www.google.com/maps/search/?api=1&query=JPJ+Kuching"
+  },
+  {
+    id: "kuching-sarawak-tourism-board",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "tourism",
+    typeLabel: "Tourism",
+    name: "Sarawak Tourism Board",
+    hours: "Mon – Fri · Office Hours",
+    phone: "",
+    website: "https://sarawaktourism.com",
+    map: "https://www.google.com/maps/search/?api=1&query=Sarawak+Tourism+Board+Kuching"
+  },
+  {
+    id: "kuching-city-council",
+    city: "kuching",
+    cityLabel: "Kuching",
+    type: "council",
+    typeLabel: "City Council",
+    name: "Kuching City Council",
+    hours: "Mon – Fri · Office Hours",
+    phone: "",
+    website: "",
+    map: "https://www.google.com/maps/search/?api=1&query=Kuching+City+Council"
+  },
+
+  {
+    id: "sibu-hospital",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "hospital",
+    typeLabel: "Hospital",
+    name: "Sibu Hospital",
+    hours: "24 Hours",
+    phone: "084-343333",
+    website: "",
+    map: "https://www.google.com/maps/search/?api=1&query=Sibu+Hospital"
+  },
+  {
+    id: "sibu-municipal-council",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "council",
+    typeLabel: "Municipal Council",
+    name: "Sibu Municipal Council",
+    hours: "Mon – Fri · Office Hours",
+    phone: "",
+    website: "",
+    map: "https://www.google.com/maps/search/?api=1&query=Sibu+Municipal+Council"
+  },
+  {
+    id: "sibu-immigration-office",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "immigration",
+    typeLabel: "Immigration",
+    name: "Sibu Immigration Office",
+    hours: "Mon – Fri · Office Hours",
+    phone: "",
+    website: "https://imi.gov.my",
+    map: "https://www.google.com/maps/search/?api=1&query=Sibu+Immigration+Office"
+  },
+  {
+    id: "sibu-jpj",
+    city: "sibu",
+    cityLabel: "Sibu",
+    type: "jpj",
+    typeLabel: "JPJ",
+    name: "JPJ Sibu",
+    hours: "Mon – Fri · Office Hours",
+    phone: "",
+    website: "https://www.jpj.gov.my",
+    map: "https://www.google.com/maps/search/?api=1&query=JPJ+Sibu"
+  }
+];
