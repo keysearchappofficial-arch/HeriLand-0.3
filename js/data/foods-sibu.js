7817a65c675b04c9eb5eb136e68076046450f7e5
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-10 12:38:53 +0800

    Create foods-sibu.js

---
+++ js/data/foods-sibu.js
@@ -0,0 +1,29 @@
+import { img } from "./shared.js";
+
+export const sibuFoods = [
+  {
+    id: "sibu-kampua",
+    city: "sibu",
+
+    name: "Kampua Mee",
+
+    location: "Sibu",
+
+    tag: "#福州乾麵",
+
+    image: img.food
+  },
+
+  {
+    id: "sibu-kompia",
+    city: "sibu",
+
+    name: "Kompia",
+
+    location: "Sibu",
+
+    tag: "#福州光餅",
+
+    image: img.food
+  }
+];
