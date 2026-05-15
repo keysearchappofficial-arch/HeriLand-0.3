86dd5ba41023c890ca080429c5da7edc5b4567cc
keysearchappofficial-arch <keysearchapp.official@gmail.com>
2026-05-15 09:36:06 +0800

    Create explore.js

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+const prevBtn = document.querySelector(".nav-prev");
+const nextBtn = document.querySelector(".nav-next");
+
+prevBtn?.addEventListener("click", () => {
+  console.log("prev");
+});
+
+nextBtn?.addEventListener("click", () => {
+  console.log("next");
+});
